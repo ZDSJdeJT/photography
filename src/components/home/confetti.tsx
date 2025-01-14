f6ce7c9d8f27a2ee30303e2949e32733560ef6f2
ZDSJdeJT <2240883644@qq.com>
2025-01-14 21:12:07 +0800

    礼花样式调整

--- src/components/home/confetti.tsx
+++ src/components/home/confetti.tsx
@@ -3,16 +3,19 @@
 import { useEffect } from 'react';
 import confetti from 'canvas-confetti';
 
+import { randomInRange } from '@/lib/utils';
+
 const Confetti = () => {
   useEffect(() => {
     confetti({
-      particleCount: 100,
-      spread: 70,
+      angle: randomInRange(55, 125),
+      spread: randomInRange(50, 70),
+      particleCount: randomInRange(50, 100),
       origin: { y: 0.6 },
     });
   }, []);
 
-  return <></>;
+  return null;
 };
 
 export { Confetti };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,7 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const randomInRange = (min: number, max: number) => {
+  return Math.random() * (max - min) + min;
+};
