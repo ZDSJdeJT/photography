a3e7799d81c3603f78bee8ef3de64fd30e7da66a
ZDSJdeJT <2240883644@qq.com>
2025-03-13 01:44:14 +0800

    fix: 替换依赖

--- src/components/home/confetti.tsx
+++ src/components/home/confetti.tsx
@@ -1,27 +1,33 @@
 'use client';
 
-import { useEffect } from 'react';
-import confetti from 'canvas-confetti';
+import Fireworks from 'react-canvas-confetti/dist/presets/fireworks';
 
 import { randomInRange } from '@/lib/utils';
 
 const Confetti = () => {
-  useEffect(() => {
-    confetti({
-      angle: randomInRange(55, 125),
-      spread: randomInRange(50, 70),
-      particleCount: randomInRange(50, 100),
-      origin: { y: 0.6 },
-      colors: ['primary', 'secondary', 'muted', 'accent', 'destructive'].map(
-        (item) =>
-          getComputedStyle(document.documentElement).getPropertyValue(
-            `--${item}`,
+  return (
+    <Fireworks
+      onInit={({ confetti }) => {
+        confetti({
+          angle: randomInRange(55, 125),
+          spread: randomInRange(50, 70),
+          particleCount: randomInRange(50, 100),
+          origin: { y: 0.6 },
+          colors: [
+            'primary',
+            'secondary',
+            'muted',
+            'accent',
+            'destructive',
+          ].map((item) =>
+            getComputedStyle(document.documentElement).getPropertyValue(
+              `--${item}`,
+            ),
           ),
-      ),
-    });
-  }, []);
-
-  return null;
+        });
+      }}
+    />
+  );
 };
 
 export { Confetti };
